22fbf12908265b056bc1c7db7c6bff2269ce87a7
kimseulbi <ksb5166@gmail.com>
2018-10-25 12:07:23 +0900

    기존 js파일 제거

--- src/index.js
+++ src/index.js
@@ -1,114 +1,22 @@
-let boardState = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0]
-];
+// 숫자야구 게임은 상대방이 속으로 생각한 숫자를 맞히는 게임입니다. 이 때의 숫자는 세 자리 숫자로, 각 자리는 0에서 부터 9까지 사용할 수 있으며, 중복된 자릿수는 허용하지 않습니다.
+// 게임 방식은 스무고개와 비슷합니다. 플레이어는 9회까지 숫자를 제시할 수 있으며, 숫자가 존재하지만 자리가 틀렸을 때는 볼, 숫자의 자리까지 일치하면 스트라이크, 일치하는 숫자가 전혀 없으면 아웃이라는 결과가 나옵니다. 예를 들어, 속으로 생각한 숫자가 123이라고 하면,
+// 과 같은 결과가 나옵니다. 3 스트라이크가 나오면 플레이어가 이기게 됩니다. 9회까지 숫자를 제시했는데도 맞히지 못하면, 플레이어가 지게 됩니다.
 
-const restartEl = document.querySelector(".reset");
-const resultEl = document.querySelector(".result");
+// + input 3개의 숫자를 입력 받음
+// + 세자리 숫자는 0부터 9 까지, 중복되지 않음
+// + 상대방이 생각하는 숫자가 램덤으로 생성
+// + 존재하지 않고 자리가 틀리면 out
+// + 존재하고 자리가 틀리면 B
+// + 존재하고 자리도 맞으면 S
+// + 시도 버튼을 눌러 확인
+// + 다시시작 버튼을 눌러 초기화
+
+//렌덤 정답 생성하는 코드
+
+//정답을 입력된 숫자와 비교하는 코드
+
+//ball, strike를 알려주는 코드
+
+//맞았다, 틀렸다를 알려주는 코드
 
 //상태를 그려주는 코드
-function drawBoard() {
-  document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
-    rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
-      if (boardState[rowIndex][colIndex] === 1) {
-        colEl.classList.add("checked");
-      } else {
-        colEl.classList.remove("checked");
-      }
-    });
-  });
-  if (bingo(boardState)) {
-    resultEl.textContent = "빙고";
-    document.querySelector(".reset").classList.add("show");
-  } else {
-    resultEl.textContent = "";
-    document.querySelector(".reset").classList.remove("show");
-  }
-}
-
-//재시작 버튼 동작 코드
-restartEl.addEventListener("click", e => {
-  boardState = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-  ];
-  drawBoard();
-});
-
-function bingo(arr) {
-  // 가로줄 확인 (루프)
-  for (let i = 0; i < 5; i++) {
-    // '이제까지 본 것이 전부 x표시가 되어있다'
-    let checked = true;
-    for (let j = 0; j < 5; j++) {
-      if (arr[i][j] === 0) {
-        checked = false;
-      }
-    }
-    if (checked) {
-      return true;
-    }
-  }
-
-  // 세로줄 확인 (루프)
-  for (let i = 0; i < 5; i++) {
-    // '이제까지 본 것이 전부 x표시가 되어있다'
-    let checked = true;
-    for (let j = 0; j < 5; j++) {
-      if (arr[j][i] === 0) {
-        checked = false;
-      }
-    }
-    if (checked) {
-      return true;
-    }
-  }
-
-  {
-    // 대각선 확인 (루프)
-    let checked = true;
-    for (let j = 0; j < 5; j++) {
-      if (arr[j][j] === 0) {
-        checked = false;
-      }
-    }
-    if (checked) {
-      return true;
-    }
-  }
-
-  {
-    // 반대쪽 대각선 확인 (루프)
-    let checked = true;
-    for (let j = 0; j < 5; j++) {
-      if (arr[j][4 - j] === 0) {
-        checked = false;
-      }
-    }
-    if (checked) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-//
-document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
-  rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
-    colEl.addEventListener("click", e => {
-      if (!bingo(boardState)) {
-        boardState[rowIndex][colIndex] = 1;
-        drawBoard();
-      }
-    });
-  });
-});
-
-drawBoard();
